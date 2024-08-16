e1eb7dd3b50340c14f0de3c8cc4faeed1fa15ba7
Szer13 <shad350@gmail.com>
2024-08-16 16:47:59 +0200

    added SwaggerConfig to enable queries to be executed

---
+++ src/main/java/com/shair13/external_service/configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.shair13.external_service.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080/api").description("Local Server")
+                ));
+    }
+}
